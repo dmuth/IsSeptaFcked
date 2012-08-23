3716ff6c992423b99a894e1fc415592ad2d40c6a
Douglas Muth <dmuth@dmuth.org>
2012-08-22 23:24:33 -0400

    Added /api endpoint.

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 
 var express = require('express');
 
+
+
+// TODO:
+//   request: headers: 'x-forwarded-for': 'real IP address'
 var app = express.createServer(express.logger());
+
+var septa = require("./lib/septa/main.js");
+
 
 //
 // Associative array for all of our routes
 //
 var routes = {};
+routes["api"] = require("./routes/api.js");
 routes["echo"] = require("./routes/echo.js");
 
 
@@ -14,8 +22,8 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get("/api", routes["api"].go);
 app.get("/echo", routes["echo"].go);
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
@@ -23,6 +31,9 @@ app.listen(port, function() {
 });
 
 
-
+//
+// Start up the SEPTA sub-system, specifically fetching from the API.
+//
+septa.boot();
 
 
