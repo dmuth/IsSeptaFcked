2f4698a2dc6e3386060ad5ebeb15974fcc470686
Douglas Muth <dmuth@dmuth.org>
2012-08-27 22:59:49 -0400

    Refactored logger code into a separate module.

--- web.js
+++ web.js
@@ -1,46 +1,12 @@
 
 var express = require('express');
+var logger = require("./lib/logger.js");
 
 //
-// Create an IP token for the logging system that lists the original IP, 
-// if there was a proxy involved.
+// Install our custom logger so we can get the source IP behind a proxy.
 //
-express.logger.token("ip", function(request) {
+logger.go(express.logger);
 
-	var retval = "";
-
-	if (request["headers"] && request["headers"]["x-forwarded-for"]) {
-		//
-		// Proxied request
-		//
-		retval = request["headers"]["x-forwarded-for"];
-
-	} else if (request["socket"] && request["socket"]["remoteAddress"]) {
-		//
-		// Direct request
-		//
-		retval = request["socket"]["remoteAddress"];
-
-	} else if (request["socket"] && request["socket"]["socket"] 
-		&& request["socket"]["socket"]["remoteAddress"]) {
-		//
-		// God only knows what happened here...
-		//
-		retval = request["socket"]["socket"]["remoteAddress"];
-
-	}
-	
-	return(retval);
-
-});
-
-
-//
-// Tweak our default logging format to include the new IP token.
-//
-express.logger.format("default", 
-	':ip :remote-addr - - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
-	);
 
 var app = express.createServer(express.logger());
 var seq = require("seq");
