bde5c95ac730c366ce2f09be77129b5a855e73f6
Douglas Muth <dmuth@dmuth.org>
2012-08-27 22:32:50 -0400

    Refactored code for the front page.

---
+++ routes/main.js
@@ -0,0 +1,113 @@
+/**
+* This module handles rendering of our front page.
+*
+* @author Douglas Muth <http://www.dmuth.org/>
+*
+*/
+
+var seq = require("seq");
+var util = require("util");
+var septa = require("../lib/septa/main.js");
+
+
+exports.go = function(request, response) {
+
+	seq().seq(function() {
+		septa.getData(this);
+
+	}).seq(function(data) {
+		var status = data["trains"]["status"];
+
+		var message = "";
+
+		var time_t = Math.round(new Date().getTime() / 1000);
+		var age = time_t - data["trains"]["time_t"];
+		var max_age = 60 * 10;
+		//var max_age = 1; // Debugging
+
+		//
+		// If our data is too old, then we're going to ignore it.
+		//
+		if (age > max_age) {
+			status["status"] = "(unknown)";
+			status["late"] = [];
+		}
+
+		var status_class = getStatusClass(status["status"]);
+		var message = getMessage(status["status"], max_age);
+
+		//
+		// Jade documentation can be found at:
+		// https://github.com/visionmedia/jade
+		//
+		response.render("index.jade", {
+				"title": "Is SEPTA Fucked?",
+				"status": status["status"],
+				"status_class": status_class,
+				"late": status["late"],
+				"message": message,
+			});
+
+	}).catch(function(error) {
+		console.log("ERROR: web.js: /: " + error);
+
+	});
+
+} // End of go()
+
+
+/**
+* This function checks our status, and decides what CSS class to tell our 
+* template to use.
+*/
+function getStatusClass(status) {
+
+	var retval = "status-unknown";
+	if (status == "not fucked") {
+		retval = "status-not-fcked";
+
+	} else if (status == "a little fucked") {
+		retval = "status-a-little-fcked";
+
+	} else if (status == "fucked") {
+			retval = "status-fcked";
+
+	} 
+
+	return(retval);
+
+} // End of getStatusClass()
+
+
+/**
+* Get our message to display, based on our current status.
+*/
+function getMessage(status, max_age) {
+
+	var retval = "";
+
+	if (status == "(unknown)") {
+		var minutes = Math.round(max_age / 60);
+		retval = util.format(
+				"Our last successful data from SEPTA is over %d minutes old. "
+				+ " We're not sure what's going on. "
+				+ "Please try refreshing this page again shortly.",
+				minutes
+				);
+
+	} else if (status == "fucked") {
+		retval = "You may want to look into alternate forms of transportation.";
+
+	} else if (status == "a little fucked") {
+		retval = "Check back here in a few minutes to see if things improve.";
+
+	} else if (status == "not fucked") {
+		retval = "All trains are running on or close to on time!";
+
+	}
+
+	return(retval);
+
+} // End of getMessage()
+
+
--- web.js
+++ web.js
@@ -14,10 +14,12 @@ var util = require("util");
 // Associative array for all of our routes
 //
 var routes = {};
+routes["main"] = require("./routes/main.js");
 routes["api"] = require("./routes/api.js");
 routes["api_raw"] = require("./routes/api_raw.js");
 routes["echo"] = require("./routes/echo.js");
 
+app.get("/", routes["main"].go);
 app.get("/api", routes["api"].go);
 app.get("/api/raw", routes["api_raw"].go);
 app.get("/echo", routes["echo"].go);
@@ -32,71 +34,6 @@ app.use(express.static(__dirname + '/public'));
 // Set our Views directory for Jade.
 //
 app.set("views", __dirname + "/views");
-
-app.get("/", function(request, response) {
-
-	seq().seq(function() {
-		septa.getData(this);
-
-	}).seq(function(data) {
-		var status = data["trains"]["status"];
-
-		var message = "";
-
-		var time_t = Math.round(new Date().getTime() / 1000);
-		var age = time_t - data["trains"]["time_t"];
-		var max_age = 60 * 10;
-		//var max_age = 1; // Debugging
-
-		//
-		// If our data is too old, then we're going to ignore it.
-		//
-		if (age > max_age) {
-			status["status"] = "(unknown)";
-			status["late"] = [];
-		}
-
-
-		var status_class = "status-unknown";
-		if (status["status"] == "not fucked") {
-			status_class = "status-not-fcked";
-
-		} else if (status["status"] == "a little fucked") {
-			status_class = "status-a-little-fcked";
-
-		} else if (status["status"] == "fucked") {
-			status_class = "status-fcked";
-
-		} else {
-			var minutes = Math.round(max_age / 60);
-			message = 
-				util.format(
-					"Our last successful data from SEPTA is over %d minutes old. "
-					+ " We're not sure what's going on. "
-					+ "Please try refreshing this page again shortly.",
-					minutes
-					);
-
-		}
-
-		//
-		// Jade documentation can be found at:
-		// https://github.com/visionmedia/jade
-		//
-		response.render("index.jade", {
-				"title": "Is SEPTA Fucked?",
-				"status": status["status"],
-				"status_class": status_class,
-				"late": status["late"],
-				"message": message,
-			});
-
-	}).catch(function(error) {
-		console.log("ERROR: web.js: /: " + error);
-
-	});
-
-});
 
 
 //
