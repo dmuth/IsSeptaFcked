2762d02fa798a93e79833098514151cb93886496
Douglas Muth <dmuth@dmuth.org>
2012-10-02 22:48:21 -0400

    Added code for displaying bus data.

---
+++ routes/api-bus-raw.js
@@ -0,0 +1,41 @@
+/**
+* This module provides our external API functionality.
+*
+* @author Douglas Muth <http://www.dmuth.org/>
+*
+*/
+
+
+var septa = require("../lib/septa/bus/main.js");
+var util = require("util");
+
+
+/**
+* This function is our main entry point.
+*/
+exports.go = function(request, response) {
+
+	var retval = "";
+
+	septa.getRawData(function(error, in_data) {
+
+		var data = {};
+		data["data"] = in_data;
+		data["_comment"] = "Raw bus data from SEPTA";
+
+		retval += JSON.stringify(data, null, 4);
+
+		//
+		// Make this readable in the web browser.
+		//
+		response.header("Content-Type", "text/json");
+
+		response.send(retval);
+
+	});
+
+} // End of go()
+
+
+
+
