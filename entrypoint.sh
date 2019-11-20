9c0c0296c8d1e47666856adbdf17fb87230db674
Douglas Muth <dmuth@dmuth.org>
2019-11-20 18:42:43 -0500

    Tweaked so that npm init is run before bash.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,22 @@
 # Errors are fatal
 set -e
 
+echo "# "
+echo "# Installing modules"
+echo "# "
+npm install
+
 if test "$1"
 then
 	echo "# "
 	echo "# Argument specified! Executing command: $@"
 	echo "# "
+	echo "# Run 'npm start' to start the website."
+	echo "# "
 	exec $@
 fi
 
 cd /mnt
-
-echo "# "
-echo "# Installing modules"
-echo "# "
-npm install
 
 echo "# "
 echo "# Running 'npm start'"
